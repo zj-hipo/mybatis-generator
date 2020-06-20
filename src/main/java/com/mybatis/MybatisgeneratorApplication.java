fc088eae46ca6afbb35f16eadbdbebf759835ce8
zj-hipo <542699293@qq.com>
2020-06-20 10:40:34 +0800

    mybatis的逆向工程

---
+++ src/main/java/com/mybatis/MybatisgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisgeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mybatis/MybatisgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
